504ebb00c8592de63180677fa9fc34500dc42e85
Jokiko <75935156+Jokiko@users.noreply.github.com>
2023-06-13 15:42:59 +0200

    Camera Movement Finalized
    -removed script from oak_tree in assignemnt F
    -added camera script to camera in assignment G
    -added first and third person view to camera script

--- Assets/Scripts/camera_movement.cs
+++ Assets/Scripts/camera_movement.cs
@@ -4,19 +4,31 @@ using UnityEngine;
 
 public class camera_movement : MonoBehaviour
 {
-    private int cameraMode = 0; //0 = standard, 1 = 1st person, 2 = 3rd person, 3 = bird's eye view
+    private int cameraMode = 0; // 0 = 1st person, 1 = 3rd person, 2 = bird's eye view
 
-    private Vector3 originalPosition;
-    private Quaternion originalRotation;
+    /*private Vector3 originalPosition;
+    private Quaternion originalRotation;*/
+
+    private Transform target;
+    private float smoothSpeed = 10f;
+    private Vector3 firstPersonOffset;
+
+    private float distance = 4f;  // 
+    private float height = 2f;  // 
+    private Vector3 thirdPersonOffset;
 
     private float birdsEyeHeight = 40f;
 
     // Start is called before the first frame update
     void Start()
     {
-        //Saving original camera position to reset it in cameraMode 0 
-        originalPosition = transform.position;
-        originalRotation = transform.rotation;
+        //Saving original camera position to reset camera
+        /*originalPosition = transform.position;
+        originalRotation = transform.rotation;*/
+
+        target = GameObject.Find("Sparrow").transform;
+        firstPersonOffset = new Vector3(0f, 0f, 0f);
+        thirdPersonOffset = new Vector3(0f, height, distance);
     }
 
     // Update is called once per frame
@@ -24,19 +36,41 @@ public class camera_movement : MonoBehaviour
     {
         if (Input.GetKeyDown(KeyCode.Space))
         {
-            cameraMode = (cameraMode + 1) % 4; 
+            cameraMode = (cameraMode + 1) % 3; 
         }
 
         switch(cameraMode){
+            /*//Standard
             case 0:
                 transform.position = originalPosition;
                 transform.rotation = originalRotation;
+                break;*/
+            //First Person    
+            case 0: 
+                Vector3 desiredPosition = target.position + firstPersonOffset;
+
+                Vector3 smoothedPosition = Vector3.Lerp(transform.position, desiredPosition, smoothSpeed * Time.deltaTime);
+                transform.position = smoothedPosition;
+
+                //turn camera in rotation of bird
+                transform.rotation = target.rotation;
                 break;
-            case 1: 
+            //Third Person    
+            case 1:
+                Vector3 newPosition = target.position - (target.forward * thirdPersonOffset.z) + (Vector3.up * thirdPersonOffset.y);
+
+                transform.position = Vector3.Lerp(transform.position, newPosition, smoothSpeed * Time.deltaTime);
+                Vector3 lookDirection = target.forward;
+
+                // calculate rotation based on bird's forward rotation
+                Vector3 lookAtPosition = target.position + (target.forward * thirdPersonOffset.z);
+
+                // turn camera where the bird is looking
+                transform.LookAt(lookAtPosition);
                 break;
+            //Bird's Eye View    
             case 2:
-                break;
-            case 3:
+                //put camera in the sky
                 Vector3 cameraPosition = transform.position;
                 cameraPosition.y = birdsEyeHeight;
                 transform.position = cameraPosition;
